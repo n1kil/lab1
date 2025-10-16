b0a396c80bd72a9550869f2b15ddc459a7f87c67
n1kil <nikchernyavsky@bk.ru>
2025-10-16 13:04:54 +1000

    add most of requirments

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .forms import UserForm
 
 
 def index(request):
-    return render(request, "index.html")
+    articles = [
+        {'title': 'Основы Python', 'date': timezone.datetime(2024, 1, 15)},
+        {'title': 'Сегодняшняя статья', 'date': timezone.now()},
+        {'title': 'Базы данных в Django', 'date': timezone.datetime(2024, 1, 5)},
+        {'title': 'fdfdf', 'date': timezone.now()},
+    ]
+    
+    context = {
+        'articles': articles,
+        'today': timezone.now().date()  
+    }
+    return render(request, 'index.html', context)
+
 
 def about(request):
     return render(request, "about.html")
 
 def contact(request):
     return render(request, "contact.html")
+
+def news(request, number):
+    return HttpResponse(f"<h1>Статья номер {number}</h1>")
+
+def feedback(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        mail = request.POST.get("mail")
+        message = request.POST.get("message")
+        context = {
+            "form": UserForm(),  
+            "submitted_data": {
+                "name": name,
+                "mail": mail,
+                "message": message
+            }
+        }
+        return render(request, "feedback.html", context)
+    else:
+        userform = UserForm()
+        return render(request, "feedback.html", {"form": userform})
