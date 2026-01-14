ca64b6e4de053576983922f3cb4d1d2ea4766a5a
n1kil <nikchernyavsky@bk.ru>
2026-01-14 18:01:05 +1000

    old version

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,26 +7,3 @@ class UserForm(forms.Form):
     mail = forms.EmailField()
     message = forms.CharField()
 
-class ArticleForm(forms.ModelForm):
-    class Meta:
-        model = Article
-        fields = ['title', 'text', 'category']
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
-            'category': forms.Select(attrs={'class': 'form-control'}),
-        }
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['author_name', 'text']
-        widgets = {
-            'author_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-        }
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'name', 'email', 'password1', 'password2']
